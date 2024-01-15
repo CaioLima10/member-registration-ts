7420e6673c80d2fc36b37ed3e787735bb1c08205
Caio lima <caiohenrys50@gmail.com>
2024-01-14 23:46:33 -0300

    feat: add post, get and delete

--- src/app/api/member/route.ts
+++ src/app/api/member/route.ts
@@ -3,6 +3,46 @@ import prismaClient from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
+
+export  async function DELETE( request: Request ){
+
+  const session = await getServerSession(authOptions)
+
+  if(!session || !session.user){
+    return NextResponse.json({ error: "Not authorized"}, { status: 401 })
+  }
+
+  const { searchParams } = new URL(request.url)
+  const userId = searchParams.get("id")
+
+  if(!userId){
+    return NextResponse.json({ error: "Not authorized"}, { status: 401 })
+  }
+
+  const memberTicket = await prismaClient.member.findFirst({
+    where: {
+      id: userId
+    }
+  })
+
+  if(memberTicket){
+    return NextResponse.json({ error: "failed delete member" }, { status: 401 })
+  }
+
+  try {
+    await prismaClient.customer.delete({
+      where: {
+        id: userId as string
+      }
+    })
+
+    return NextResponse.json({ message: "Membro deletado com sucesso!" })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ error: "Not authorized"}, { status: 401 })
+  }
+} 
+
 export async function POST(request: Request){
     const session = await getServerSession(authOptions)
 
@@ -36,3 +76,5 @@ export async function POST(request: Request){
       return NextResponse.json({ error: "failed created new member"}, { status: 400 })
     }
 }
+
+

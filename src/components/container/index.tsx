cd4c2bf103ee1bc7244de261106460e68f0bdcbb
Caio lima <caiohenrys50@gmail.com>
2024-01-15 23:43:26 -0300

    feat: component container

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -4,7 +4,7 @@ export default function Container({children}: {children: ReactNode} ) {
   return (
     <div 
       className="flex items-center w-full md:w-3/4 mx-auto 
-                  h-20 px-4 bg-gray-950 text-gray-100">
+                  my-4">
       {children}
     </div>
   )

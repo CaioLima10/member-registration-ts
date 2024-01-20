536b735b6eaee89d8673a80542c1ffe10f2e3f82
Caio lima <caiohenrys50@gmail.com>
2024-01-19 23:56:57 -0300

    feat: types cards

---
+++ src/utils/member.type.ts
@@ -0,0 +1,13 @@
+export interface ICardMembersProps {
+    id: string
+    name: string
+    email: string
+    phone: string
+    address: string | null
+    cpf: string | null
+    job: string | null
+    registerRG: string | null
+    complement: string | null
+    houseNumber: string | null
+  }
+  

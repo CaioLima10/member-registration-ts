ce4f1acbdfcf92890f6184f315e8866bf4288cfd
Caio lima <caiohenrys50@gmail.com>
2024-02-22 23:52:15 -0300

    config: experimental

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    domains: ["lh3.googleusercontent.com"],
+  experimental: {
+    images: {
+      domains: ["lh3.googleusercontent.com"],
+    },
   },
 };
 
